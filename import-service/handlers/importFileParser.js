cbbfa8c80ec129776ae88d25af28f41ce2f12ff8
Aleh Brytvich <Aleh_Brytvich@epam.com>
2022-11-11 05:55:06 -0600

    Test-6

--- import-service/handlers/importFileParser.js
+++ import-service/handlers/importFileParser.js
@@ -1,23 +1,43 @@
 const aws = require("aws-sdk");
-const csv = require('csv-parser');
+const { parse } = require('csv-parse');
+const { v4: uuidv4 } = require('uuid');
 
-const s3 = new aws.S3({region: 'eu-west-1'});
-const bucket = "cloudx-nodejs-backend-import-bucket2"
-const headers = {'Access-Control-Allow-Origin': '*'}
+const region = 'eu-west-1';
+aws.config.update({region: region});
+const s3 = new aws.S3({region: region});
+const bucket = "cloudx-nodejs-backend-import-bucket2";
+const headers = {'Access-Control-Allow-Origin': '*'};
+const SQS = new aws.SQS({apiVersion: '2012-11-05'})
 
-  function parseFile(fileName) {
+function sendToQueue(data) {
+  const params = {
+    MessageBody: data.toString(),
+    QueueUrl: "https://sqs.eu-west-1.amazonaws.com/007756198797/catalogItemsQueue",
+  }
+  console.log(data.toString());
+  SQS.sendMessage(params, (err, data) => {
+    if (err) {
+      console.log("Error", err);
+    } else {
+      console.log("Successfully added message", data.MessageId);
+    }
+  });
+}
+
+
+function parseFile(fileName) {
   const s3Params = {
     Bucket: bucket,
     Key: fileName,
   }
 
   return new Promise((resolve) => {
-    let results = [];
-    s3.getObject(s3Params).createReadStream().pipe(csv())
-      .on('data', (data) => results.push(data))
+    s3.getObject(s3Params).createReadStream().pipe(parse({
+      delimiter: ','
+    }))
+      .on('data', (data) => sendToQueue(data))
       .on('end', () => {
-        console.log(results);
-        return resolve('Success: ' + results)
+        return resolve('Success: ')
       });
   });
 }
